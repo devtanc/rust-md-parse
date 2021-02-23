a5b766fad365087eff74428b5573613883c0a397
devtanc <devtanc@gmail.com>
2021-02-23 00:44:41 -0700

    add more types and create Token tuple struct

--- src/main.rs
+++ src/main.rs
@@ -6,28 +6,42 @@ use std::io::{BufWriter, Write};
 #[derive(Debug)]
 #[derive(Copy, Clone)]
 enum TokenType {
+  Ampersand,
+  At,
   Asterisk,
   Backslash,
   Backtick,
   CarriageReturn,
+  Colon,
+  Comma,
   CurlyBracketClose,
   CurlyBracketOpen,
+  Digit,
   Dot,
+  Dollar,
   DoubleQuotes,
+  Equals,
   EOF,
   Exclamation,
+  ForwardSlash,
+  GT,
   Hashtag,
+  LT,
   Letter,
   Minus,
   Newline,
   ParenthesisClose,
   ParenthesisOpen,
+  Pipe,
   Plus,
+  QuestionMark,
+  Semicolon,
   SingleQuote,
   Space,
   SquareBracketClose,
   SquareBracketOpen,
   Tab,
+  Tilde,
   Uncategorized,
   Underscore,
   Word,
@@ -38,6 +52,10 @@ impl fmt::Display for TokenType {
     fmt::Debug::fmt(self, f)
   }
 }
+
+#[derive(Debug)]
+#[derive(Clone)]
+struct Token(TokenType, String, usize);
 
 fn main() {
   let args: Vec<String> = env::args().collect();
@@ -51,54 +69,68 @@ fn main() {
   print_to_file(filename, result);
 }
 
-fn tokenize_contents(contents: &str) -> Vec<(TokenType, String, usize)> {
-  let mut tokens: Vec<(TokenType, String, usize)> = Vec::new();
+fn tokenize_contents(contents: &str) -> Vec<Token> {
+  let mut tokens: Vec<Token> = Vec::new();
 
   for (i, character) in contents.chars().enumerate() {
     match character {
       // Special characters
-      '*' => tokens.push((TokenType::Asterisk, String::from(character), i)),
-      '`' => tokens.push((TokenType::Backtick, String::from(character), i)),
-      '\\' => tokens.push((TokenType::Backslash, String::from(character), i)),
-      '}' => tokens.push((TokenType::CurlyBracketClose, String::from(character), i)),
-      '{' => tokens.push((TokenType::CurlyBracketOpen, String::from(character), i)),
-      '.' => tokens.push((TokenType::Dot, String::from(character), i)),
-      '"' => tokens.push((TokenType::DoubleQuotes, String::from(character), i)),
-      '!' => tokens.push((TokenType::Exclamation, String::from(character), i)),
-      '#' => tokens.push((TokenType::Hashtag, String::from(character), i)),
-      '-' => tokens.push((TokenType::Minus, String::from(character), i)),
-      ')' => tokens.push((TokenType::ParenthesisClose, String::from(character), i)),
-      '(' => tokens.push((TokenType::ParenthesisOpen, String::from(character), i)),
-      '+' => tokens.push((TokenType::Plus, String::from(character), i)),
-      '\'' => tokens.push((TokenType::SingleQuote, String::from(character), i)),
-      ']' => tokens.push((TokenType::SquareBracketClose, String::from(character), i)),
-      '[' => tokens.push((TokenType::SquareBracketOpen, String::from(character), i)),
-      '_' => tokens.push((TokenType::Underscore, String::from(character), i)),
+      '&' => tokens.push(Token(TokenType::Ampersand, String::from(character), i)),
+      '@' => tokens.push(Token(TokenType::At, String::from(character), i)),
+      '*' => tokens.push(Token(TokenType::Asterisk, String::from(character), i)),
+      '`' => tokens.push(Token(TokenType::Backtick, String::from(character), i)),
+      '\\' => tokens.push(Token(TokenType::Backslash, String::from(character), i)),
+      ':' => tokens.push(Token(TokenType::Colon, String::from(character), i)),
+      ',' => tokens.push(Token(TokenType::Comma, String::from(character), i)),
+      '}' => tokens.push(Token(TokenType::CurlyBracketClose, String::from(character), i)),
+      '{' => tokens.push(Token(TokenType::CurlyBracketOpen, String::from(character), i)),
+      '.' => tokens.push(Token(TokenType::Dot, String::from(character), i)),
+      '$' => tokens.push(Token(TokenType::Dollar, String::from(character), i)),
+      '"' => tokens.push(Token(TokenType::DoubleQuotes, String::from(character), i)),
+      '=' => tokens.push(Token(TokenType::Equals, String::from(character), i)),
+      '!' => tokens.push(Token(TokenType::Exclamation, String::from(character), i)),
+      '/' => tokens.push(Token(TokenType::ForwardSlash, String::from(character), i)),
+      '>' => tokens.push(Token(TokenType::GT, String::from(character), i)),
+      '#' => tokens.push(Token(TokenType::Hashtag, String::from(character), i)),
+      '<' => tokens.push(Token(TokenType::LT, String::from(character), i)),
+      '-' => tokens.push(Token(TokenType::Minus, String::from(character), i)),
+      ')' => tokens.push(Token(TokenType::ParenthesisClose, String::from(character), i)),
+      '(' => tokens.push(Token(TokenType::ParenthesisOpen, String::from(character), i)),
+      '|' => tokens.push(Token(TokenType::Pipe, String::from(character), i)),
+      '+' => tokens.push(Token(TokenType::Plus, String::from(character), i)),
+      '?' => tokens.push(Token(TokenType::QuestionMark, String::from(character), i)),
+      ';' => tokens.push(Token(TokenType::Semicolon, String::from(character), i)),
+      '\'' => tokens.push(Token(TokenType::SingleQuote, String::from(character), i)),
+      ']' => tokens.push(Token(TokenType::SquareBracketClose, String::from(character), i)),
+      '[' => tokens.push(Token(TokenType::SquareBracketOpen, String::from(character), i)),
+      '~' => tokens.push(Token(TokenType::Tilde, String::from(character), i)),
+      '_' => tokens.push(Token(TokenType::Underscore, String::from(character), i)),
       // Whitespace
-      ' ' => tokens.push((TokenType::Space, String::from(character), i)),
-      '\n' => tokens.push((TokenType::Newline, String::from(character), i)),
-      '\t' => tokens.push((TokenType::Tab, String::from(character), i)),
-      '\r' => tokens.push((TokenType::CarriageReturn, String::from(character), i)),
+      ' ' => tokens.push(Token(TokenType::Space, String::from(character), i)),
+      '\n' => tokens.push(Token(TokenType::Newline, String::from(character), i)),
+      '\t' => tokens.push(Token(TokenType::Tab, String::from(character), i)),
+      '\r' => tokens.push(Token(TokenType::CarriageReturn, String::from(character), i)),
       // Letters
-      'A'..='Z' | 'a'..='z' => tokens.push((TokenType::Letter, String::from(character), i)),
+      'A'..='Z' | 'a'..='z' => tokens.push(Token(TokenType::Letter, String::from(character), i)),
+      '0'..='9' => tokens.push(Token(TokenType::Digit, String::from(character), i)),
       // Everything else
-      _ => tokens.push((TokenType::Uncategorized, String::from(character), i)),
+      _ => tokens.push(Token(TokenType::Uncategorized, String::from(character), i)),
     }
   }
 
-  tokens.push((TokenType::EOF, String::from(""), contents.chars().count()));
+  tokens.push(Token(TokenType::EOF, String::from(""), contents.chars().count()));
 
   return process_letters_into_words(tokens);
 }
 
-fn process_letters_into_words(tokenized_characters: Vec<(TokenType, String, usize)>) -> Vec<(TokenType, String, usize)> {
-  let mut processed_tokens: Vec<(TokenType, String, usize)> = Vec::new();
+fn process_letters_into_words(tokenized_characters: Vec<Token>) -> Vec<Token> {
+  let mut processed_tokens: Vec<Token> = Vec::new();
   let mut start_of_word: usize = 0;
   let mut word: String = "".to_owned();
 
   for (i, token) in tokenized_characters.iter().enumerate() {
     match token {
-      (TokenType::Letter, contents, _) => {
+      Token(TokenType::Letter, contents, _) => {
         if word.len() == 0 {
           start_of_word = i
         }
@@ -107,12 +139,11 @@ fn process_letters_into_words(tokenized_characters: Vec<(TokenType, String, usiz
       },
       _ => {
         if start_of_word > 0 || word.len() > 0 {
-          processed_tokens.push((TokenType::Word, word, start_of_word));
+          processed_tokens.push(Token(TokenType::Word, word, start_of_word));
           word = "".to_owned();
           start_of_word = 0;
         }
-        let (token_type, contents, index) = token;
-        processed_tokens.push((*token_type, contents.clone(), *index));
+        processed_tokens.push(token.clone());
       }
     }
   }
@@ -120,7 +151,7 @@ fn process_letters_into_words(tokenized_characters: Vec<(TokenType, String, usiz
   return processed_tokens;
 }
 
-fn print_to_file(filename: String, tuples: Vec<(TokenType, String, usize)>) {
+fn print_to_file(filename: String, tuples: Vec<Token>) {
   let file = fs::File::create(format!("{}.txt", filename)).expect("Unable to create file");
   let mut file_writer = BufWriter::new(file);
 
